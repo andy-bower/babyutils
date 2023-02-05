e996a6384a02721085a6869cab8ed77519e2a7f4
Andrew Bower <andrew@bower.uk>
2023-02-05 08:51:07 +0000

    add test sources

---
+++ test/test-jmp.asm
@@ -0,0 +1,31 @@
+-- Test JMP instruction
+
+  ldn dat1
+  sto check1
+  jmp todest1
+  sto check4
+  hlt
+
+dest1:
+  sto check2
+  ldn dat2
+  sto check3
+  hlt
+
+dat1:
+  num -0x11
+todest1:
+  eja dest1
+dat2:
+  num -0x22
+dat3:
+  num -0x33
+
+check1:
+  num 1 -- should become 0x11
+check2:
+  num 2 -- should become 0x11
+check3:
+  num 3 -- should become 0x22
+check4:
+  num 4 -- should stay 4
