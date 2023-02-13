aca42706e3cb3436426867f1b2675450c0b43e85
Andrew Bower <andrew@bower.uk>
2023-02-13 22:32:31 +0000

    add tests

---
+++ test/test-count31.asm
@@ -0,0 +1,18 @@
+-- # SPDX-License-Identifier: MIT
+-- # (c) Copyright 2023 Andrew Bower
+--
+-- Count 2^31 times
+
+  ldn m1
+loop:
+  sub m1
+  skn
+  jmp j_loop
+  hlt
+
+j_loop:
+  eja loop
+
+m1:
+  num -1
+
---
+++ test/test-jrp.asm
@@ -0,0 +1,35 @@
+-- # SPDX-License-Identifier: MIT
+-- # (c) Copyright 2023 Andrew Bower
+--
+-- Test JRP instruction
+
+  ldn dat1
+  sto check1
+  jrp todest1
+  sto check4
+  hlt
+
+dest1:
+  sto check2
+  ldn dat2
+  sto check3
+  hlt
+
+dat1:
+  num -0x11
+todest1:
+  eja 3
+dat2:
+  num -0x22
+dat3:
+  num -0x33
+
+28:
+check1:
+  num 1 -- should become 0x11
+check2:
+  num 2 -- should become 0x11
+check3:
+  num 3 -- should become 0x22
+check4:
+  num 4 -- should stay 4
