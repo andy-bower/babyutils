c7c957477d051c61169e87f0fbd1b4ba69ded4b9
Andrew Bower <andrew@bower.uk>
2023-02-13 09:14:53 +0000

    add infinite counter test

---
+++ test/test-inf-ctr.asm
@@ -0,0 +1,19 @@
+-- # SPDX-License-Identifier: MIT
+-- # (c) Copyright 2023 Andrew Bower
+--
+-- Test Infinite counting
+
+  ldn ctr
+loop:
+  sub m1
+  jmp j_loop
+
+j_loop:
+  eja loop
+
+ctr:
+  num 0
+
+m1:
+  num -1
+
